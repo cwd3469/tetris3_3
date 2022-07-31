1302b96204081872bda2be99d0c1523f05b128ed
cwd3469 <cwd3469@naver.com>
2022-07-31 14:08:56 +0900

    키보드 추가 임시 저장

--- src/backup/dino.ts
+++ src/backup/dino.ts
@@ -1,10 +1,18 @@
 import './style.css'
 
+//root 
 const canvas =<HTMLCanvasElement> document.getElementById('canvas')
 const ctx:CanvasRenderingContext2D|null = canvas.getContext('2d')
 
+// root 위치
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
+
+//image
+const img1 = new Image();
+img1.src = '../src/assets/cactus.png'
+const img2 = new Image();
+img2.src = '../src/assets/dinosaur.png'
 
 
 interface Position {
@@ -16,15 +24,7 @@ interface Position {
     img : HTMLImageElement
 }
 
-const img1 = new Image();
-img1.src = '../src/assets/cactus.png'
-const img2 = new Image();
-img2.src = '../src/assets/dinosaur.png'
-
-
-console.log(img1);
-console.log(img2);
-
+// 캐릭터 프로토타입 
 class Item{
     position;
     constructor(position:Position){
@@ -41,8 +41,6 @@ class Item{
     }  
 }
 
-
-
 let dinoPosition = {
     x:10,
     y:200,
@@ -56,58 +54,9 @@ const Dino = new Item(dinoPosition)
 
 Dino.draw()
 
-let jump = false;
-let timer = 0;
-let Disorders: Item[] = []
-let jumpTimer:number =  0;
-let animate: number;
-
-const frameMovement = () =>{
-    animate = requestAnimationFrame(frameMovement);
-    timer++;
-    ctx?.clearRect(0,0,canvas.width,canvas.height)
-    
-    if(timer % 60 === 0){
-        let DisorderPosition = {
-            x:500,
-            y:200,
-            width:50,
-            height:50,
-            color:'white',
-            img:img1
-        }
-        const Disorder = new Item(DisorderPosition)
-        Disorders.push(Disorder)
-    } 
-    Disorders.forEach((a,i,o)=>{
-       if(a.position.x < 0){
-            o.splice(i,1)
-       }
-       a.position.x--;
-        physicalEngine(Dino , a)
-        a.draw();
-    })
-    
-    if(jump === true){
-        Dino.position.y--;
-        jumpTimer++;
-    }
-    if(jump === false){
-        if(Dino.position.y < 200) {
-            Dino.position.y++
-        }
-    }
-    if(jumpTimer > 100){
-        jump = false;
-        jumpTimer = 0;
-    }
-    Dino.draw()
-}
-
-frameMovement()
-
+//물리엔진
 const physicalEngine = (dino:Item , disorder:Item) =>{
-    let xAxis =  disorder.position.x - (dino.position.x + dino.position.width);
+    let xAxis =  disorder.position.x - (dino.position.x + dino.position.width); 
     let yAxis = disorder.position.y - (dino.position.y + dino.position.height);
     if(xAxis < 0 && yAxis < 0){
         ctx?.clearRect(0,0,canvas.width,canvas.height);
@@ -115,8 +64,65 @@ const physicalEngine = (dino:Item , disorder:Item) =>{
     }
  }
 
+
+ // 프레임무빙
+ let jump = false;
+ let timer = 0;
+ let Disorders: Item[] = []
+ let jumpTimer:number =  0;
+ let animate: number;
+ 
+ const frameMovement = () =>{
+     animate = requestAnimationFrame(frameMovement);
+     timer++;
+     ctx?.clearRect(0,0,canvas.width,canvas.height)
+     
+     if(timer % 60 === 0){
+         let DisorderPosition = {
+             x:500,
+             y:200,
+             width:50,
+             height:50,
+             color:'white',
+             img:img1
+         }
+         const Disorder = new Item(DisorderPosition)
+         Disorders.push(Disorder)
+     } 
+     Disorders.forEach((a,i,o)=>{
+        if(a.position.x < 0){
+             o.splice(i,1)
+        }
+        a.position.x--;
+         physicalEngine(Dino , a)
+         a.draw();
+     })
+     
+     if(jump === true){
+         Dino.position.y--;
+         jumpTimer++;
+     }
+     if(jump === false){
+         if(Dino.position.y < 200) {
+             Dino.position.y++
+         }
+     }
+     if(jumpTimer > 100){
+         jump = false;
+         jumpTimer = 0;
+     }
+     Dino.draw()
+ }
+ 
+ frameMovement()
+
+
+ //키보드 조작
 document.addEventListener('keydown',(e) =>{
-    if(e.code === 'Space'){
-        jump = true
+    switch (e.code) {
+        case 'Space':
+         return jump = true
+        default:
+            break;
     }
 })
--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,6 @@ const ctx:CanvasRenderingContext2D|null = canvas.getContext('2d')
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
-
-interface Position {
-    x:number;
-    y:number;
-    width:number;
-    height:number;
-    color:string;
-    img : HTMLImageElement
-}
-
 //image
 const img1 = new Image();
 img1.src = '../src/assets/cactus.png'
@@ -25,89 +15,45 @@ const img2 = new Image();
 img2.src = '../src/assets/dinosaur.png'
 
 
+interface Position {
+    x:number;
+    y:number;
+    width:number;
+    height:number;
+    color:string;
+  
+}
+
+// 캐릭터 프로토타입 
 class Item{
     position;
     constructor(position:Position){
       this.position = position
     }
     draw():void{
-        let { x, y, width, height, color ,img} = this.position;
-        
+        let { x, y, width, height, color} = this.position;
         if(ctx != null){
             ctx.fillStyle = color;
             ctx.fillRect(x,y,width,height);
-            ctx.drawImage(img,x,y,width,height)
         }
     }  
 }
-
-
 
 let dinoPosition = {
     x:10,
     y:200,
     width:50,
     height:50,
-    color:'white ',
-    img:img2
+    color:'green',
 }
 
 const Dino = new Item(dinoPosition)
 
 Dino.draw()
 
-let jump = false;
-let timer = 0;
-let Disorders: Item[] = []
-let jumpTimer:number =  0;
-let animate: number;
-
-const frameMovement = () =>{
-    animate = requestAnimationFrame(frameMovement);
-    timer++;
-    ctx?.clearRect(0,0,canvas.width,canvas.height)
-    
-    if(timer % 60 === 0){
-        let DisorderPosition = {
-            x:500,
-            y:200,
-            width:50,
-            height:50,
-            color:'white',
-            img:img1
-        }
-        const Disorder = new Item(DisorderPosition)
-        Disorders.push(Disorder)
-    } 
-    Disorders.forEach((a,i,o)=>{
-       if(a.position.x < 0){
-            o.splice(i,1)
-       }
-       a.position.x--;
-        physicalEngine(Dino , a)
-        a.draw();
-    })
-    
-    if(jump === true){
-        Dino.position.y--;
-        jumpTimer++;
-    }
-    if(jump === false){
-        if(Dino.position.y < 200) {
-            Dino.position.y++
-        }
-    }
-    if(jumpTimer > 100){
-        jump = false;
-        jumpTimer = 0;
-    }
-    Dino.draw()
-}
-
-frameMovement()
-
+//물리엔진
 const physicalEngine = (dino:Item , disorder:Item) =>{
-    let xAxis =  disorder.position.x - (dino.position.x + dino.position.width);
+    let xAxis =  disorder.position.x - (dino.position.x + dino.position.width); 
     let yAxis = disorder.position.y - (dino.position.y + dino.position.height);
     if(xAxis < 0 && yAxis < 0){
         ctx?.clearRect(0,0,canvas.width,canvas.height);
@@ -115,8 +61,76 @@ const physicalEngine = (dino:Item , disorder:Item) =>{
     }
  }
 
+
+ // 프레임무빙
+ let jump = false;
+ let timer = 0;
+ let Disorders: Item[] = []
+ let jumpTimer:number =  0;
+ let animate: number;
+ 
+ const frameMovement = () =>{
+     animate = requestAnimationFrame(frameMovement);
+     timer++;
+     ctx?.clearRect(0,0,canvas.width,canvas.height)
+     
+    //  if(timer % 60 === 0){
+    //      let DisorderPosition = {
+    //          x:500,
+    //          y:200,
+    //          width:50,
+    //          height:50,
+    //          color:'red',
+             
+    //      }
+    //      const Disorder = new Item(DisorderPosition)
+    //      Disorders.push(Disorder)
+    //  } 
+    //  Disorders.forEach((a,i,o)=>{
+    //     if(a.position.x < 0){
+    //          o.splice(i,1)
+    //     }
+    //     a.position.x--;
+    //      physicalEngine(Dino , a)
+    //      a.draw();
+    //  })
+     
+     if(jump === true){
+         Dino.position.y--;
+         jumpTimer++;
+     }
+     if(jump === false){
+         if(Dino.position.y < 200) {
+             Dino.position.y++
+         }
+     }
+     if(jumpTimer > 100){
+         jump = false;
+         jumpTimer = 0;
+     }
+     Dino.draw()
+ }
+ 
+ frameMovement()
+
+
+
+
+ //키보드 조작
 document.addEventListener('keydown',(e) =>{
-    if(e.code === 'Space'){
-        jump = true
-    }
+	e = e || window.event;
+	switch (e.code) {
+		case  'Space':
+			return jump = true;
+		case 'ArrowLeft' :
+			return console.log('left');
+		case 'ArrowDown' :
+			return console.log('down');
+		case 'ArrowUp' :
+			return console.log('up');
+		case 'ArrowRight' :
+			return console.log('right');
+		default:
+			break;
+	}
 })
